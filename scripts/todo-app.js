322c28de7f506b6f7cef06dfca9643d2da3e58c9
padawan95 <zeli.armel@gmail.com>
2019-11-15 17:35:51 +0100

    Add comments

--- scripts/todo-app.js
+++ scripts/todo-app.js
@@ -9,11 +9,17 @@ const filters = {
 
 renderTodos(todos, filters)
 
+/**
+ * Filter the todo by text
+ */
 document.querySelector("#search-text").addEventListener('input',  (e) => {
     filters.searchText = e.target.value
     renderTodos(todos, filters)
 })
 
+/**
+ * Add a new todo
+ */
 document.querySelector('#new-todo').addEventListener('submit', (e) => {
     e.preventDefault()
     const textTrimmed = e.target.elements.text.value.trim()
@@ -32,6 +38,9 @@ document.querySelector('#new-todo').addEventListener('submit', (e) => {
     }
 })
 
+/**
+ * Hide todo completed
+ */
 document.querySelector('#hide-completed').addEventListener('change', (e) => {
     filters.hideCompleted = e.target.checked
     renderTodos(todos,filters)
